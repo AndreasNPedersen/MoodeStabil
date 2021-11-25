541a12577fded9eaf48d7a956c13b5048d07c553
andr12f4 <75079112+andr12f4@users.noreply.github.com>
2021-11-25 12:47:06 +0100

    data start

---
+++ TestProjectMoodeStabil/UnitTestDatabase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectMoodeStabil
+{
+    [TestClass]
+    class UnitTestDatabase
+    {
+
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            PiData data = new PiData(1,DateTime.Now,DateTime.Now,1,new Subjects(1,"Programmering",DateTime.Now));
+
+
+        }
+
+    }
+}
