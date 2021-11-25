02634ec1e4288f2915ee3687e1e881952d1e784b
AlexTriumf <TriumfAlex@gmail.com>
2021-11-25 13:24:22 +0100

    til Malte han savnede den

--- TestProjectMoodeStabil/UnitTestUDP.cs
+++ TestProjectMoodeStabil/UnitTestUDP.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestProjectMoodeStabil
 {
-    class UnitTestUDP
+    [TestClass]
+    public class UnitTestUDP
     {
+        [TestMethod]
+        public void TestReciveData()
+        {
+
+            UdpClient client = new UdpClient(7);
+            byte[] data;
+            bool modtagetData = true;
+
+            while (modtagetData)
+            {
+                IPEndPoint remoteEP = null;
+
+                // modtager
+                data = client.Receive(ref remoteEP);
+
+                string str = Encoding.UTF8.GetString(data);
+                DateTime date = Convert.ToDateTime(str);
+                if (str.Length > 0)
+                {
+                    Assert.IsTrue(true);
+                    Assert.AreEqual(DateTime.Now.Hour, date.Hour);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestDataLogic()
+        {
+
+
+        }
+
+
     }
 }
