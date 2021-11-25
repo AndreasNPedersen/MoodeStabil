28b6aa94e2adc42b9698c188764a78b7bcca46d6
AlexTriumf <TriumfAlex@gmail.com>
2021-11-25 13:42:56 +0100

    Malte savner også den her

--- TestProjectMoodeStabil/UnitTestRest.cs
+++ TestProjectMoodeStabil/UnitTestRest.cs
@@ -1,13 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestProjectMoodeStabil
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestRest
     {
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void FagTest()
         {
+
+
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void TestPost()
+        {
+
+
+        }
+
+
+        [TestMethod]
+        public void FilterByDate(DateTime minDate, DateTime maxDate)
+        {
+
         }
     }
 }
