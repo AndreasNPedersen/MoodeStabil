501af02894d6afc205d191e81c547172811018b6
tobi4504 <72128467+tobi4504@users.noreply.github.com>
2021-11-25 14:11:55 +0100

    Text fejl

--- TestProjectMoodeStabil/UnitTestChagningTime.cs
+++ TestProjectMoodeStabil/UnitTestChagningTime.cs
@@ -6,11 +6,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TestProjectMoodeStabil
 {
     public class UnitTestChagningTime
     {
-        private ImanageSubjects mgr;
+        private IManageSubjects mgr;
         private  List<Subjects> subjects;
         [TestInitialize]
         public void StartUp()
