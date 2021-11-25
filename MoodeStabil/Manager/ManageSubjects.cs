89bd6f7edb2497bd7e5e8621a37385c2312e5dfb
tobi4504 <72128467+tobi4504@users.noreply.github.com>
2021-11-25 13:53:12 +0100

    Added ImanageSubject, ManageSubjects, and unit test for ImanageSubjects

---
+++ MoodeStabil/Manager/ManageSubjects.cs
@@ -0,0 +1,22 @@
+﻿using ModelLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoodeStabil.Manager
+{
+    public class ManageSubjects : IManageSubjects
+    {
+
+        public IEnumerable<Subjects> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestProjectMoodeStabil/UnitTestChagningTime.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectMoodeStabil
+{
+    public class UnitTestChagningTime
+    {
+        private ImanageSubjects mgr;
+        private  List<Subjects> subjects;
+        [TestInitialize]
+        public void StartUp()
+        {
+            // Data should be changed we real data is implemented
+            subjects = new List<Subjects>() {
+            new Subjects(2,"HE-Ho",DateTime.Now),
+            new Subjects(3,"Pe-Ho", new DateTime(1998,2,3,9,10,10)),
+            new Subjects(4,"Vi-Ho", new DateTime(1998,2,4,9,10,10)),
+            new Subjects(5,"And-Ho", new DateTime(1998,2,5,9,10,10))
+           
+        };
+        mgr = new ManageSubjects();
+        }
+        [TestMethod]
+        public void TestChagningTimeOfSubject()
+        {
+            DateTime date = new(2, 2, 2, 2, 2, 2, 2);
+            mgr.Update(2, date);
+            Subjects changedSubject = mgr.GetById(2);
+           
+            
+            Assert.AreEqual(date, changedSubject.SubjectMeetTime.Value);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestChangingTimeOfSubject()
+        {
+            mgr.update(99999999, new DateTime(2,2,2,2,2,2));
+        }
+
+    }
+}
