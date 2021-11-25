00c3dbe696116ec5ff7c38a1d94114f753830224
AlexTriumf <TriumfAlex@gmail.com>
2021-11-25 13:40:22 +0100

    til Malte for han savner det

--- ModelLib/Fag.cs
+++ ModelLib/Fag.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace ModelLib
 {
-    class Fag
+    public class Fag
     {
+        private FagEnum Navn { get; set; }
+
+        private DateTime StartTid { get; set; }
+
+        public Fag()
+        {
+
+        }
+
+        public Fag(FagEnum navn, DateTime starttid)
+        {
+            Navn = navn;
+            StartTid = starttid;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Navn)}: {Navn}, {nameof(StartTid)}: {StartTid}";
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            try
+            {
+                var otherFag = (Fag)obj;
+                if (this.Navn.Equals(otherFag.Navn) &&
+                    this.StartTid.Equals(otherFag.StartTid)) return true;
+                return false;
+            }
+            catch (InvalidCastException)
+            {
+                return false;
+            }
+        }
+
     }
+
+    public enum FagEnum
+    {
+        Teknologi,
+        Systemudvikling,
+        Programmering
+    };
+
 }
