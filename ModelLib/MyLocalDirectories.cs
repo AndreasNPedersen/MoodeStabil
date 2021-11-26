38f398e3cfd0a38b0ade6236070fffead6b7537e
DESKTOP-PV67OOQ\tobia <tobi4504@edu.easj.dk>
2021-11-26 13:06:53 +0100

    Commits

---
+++ ModelLib/MyLocalDirectories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    public class MyLocalDirectories : IMyLocalDirectories
+    {
+        private const string WebDriverDirectory = "C:\\webDrivers";
+
+        //ToDo: Link til database?
+
+        public string GetWebDriver()
+        {
+            return WebDriverDirectory;
+        }
+    }
+}
