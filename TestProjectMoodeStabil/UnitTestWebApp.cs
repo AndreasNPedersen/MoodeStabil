be19ff6c838a881153c12a82598f5bec7c60cad4
Caspar Stanley <casparstan@gmail.com>
2021-11-26 13:38:24 +0100

    Working on test driven for webapp

--- TestProjectMoodeStabil/UnitTestWebApp.cs
+++ TestProjectMoodeStabil/UnitTestWebApp.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModelLib;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
@@ -22,7 +23,13 @@ namespace TestProjectMoodeStabil
 
         static IWebDriver driver = null;
 
+        // WEB ELEMENTS
+        // Simple Elements
         static IWebElement downloadButton;
+
+        // Dropdowns
+        static IWebElement datePicker;
+        static SelectElement dateRange = new SelectElement(datePicker);
 
         [ClassInitialize]
         public static void ClassInit(TestContext context)
@@ -32,10 +39,14 @@ namespace TestProjectMoodeStabil
             //driver = new ChromeDriver(DriverDirectory);
             driver = new FirefoxDriver(DriverDirectory);
 
-            // Find Elements to test
+            // Go to website
             driver.Navigate().GoToUrl(URL);
 
-            downloadButton = driver.FindElement(By.Id("downloadButton"));
+            // Find Elements to test
+            downloadButton      = driver.FindElement(By.Id("downloadButton"));
+            datePicker          = driver.FindElement(By.Id("datePicker"));
+
+            dateRange.SelectByValue("Last 30 Days");
         }
 
         [TestInitialize]
